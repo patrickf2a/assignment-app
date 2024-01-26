ddce7dbf252c893c25645b152d59bd34630e0e22
Patrick Fernandes De Faria <47286268+patrickf2a@users.noreply.github.com>
2024-01-26 14:56:10 +0100

    TP FINAL V8  : Tout est okkkkkkkkkk

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 //Install express server
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+// Your API routes
+app.get("/api/some-route", (req, res) => {
+  // Handle the request here
+  app.route(prefix + '/assignments')
+    .get(assignment.getAssignments)
+    .post(assignment.postAssignment);
+
+  app.route(prefix + '/assignments/:id')
+    .get(assignment.getAssignment)
+    .delete(assignment.deleteAssignment)
+    .put(assignment.updateAssignment);
+
+  app.route(prefix + '/assignments/:id/rendu')
+    .put(assignment.updateRendu);
+
+  app.route(prefix + '/user')
+    .post(user.createUser);
+
+  app.route(prefix + '/user/authenticate')
+    .post(user.authenticateUser);
+
+  app.route(prefix + '/users')
+    .get(user.getUsers);
+
+});
+
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/assignment-app/"));
