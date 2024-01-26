278a3283db1cc9d8de7f200092a24f766ff63d04
Patrick Fernandes De Faria <47286268+patrickf2a@users.noreply.github.com>
2024-01-27 00:04:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 //Install express server
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
+//const cors = require("cors");
 
-const app = express();
+//const app = express();
 
-app.use(cors());
+//app.use(cors());
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/assignment-app/"));
