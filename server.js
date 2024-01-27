f097289c82a8e9d5b1c75502be6f38414e7d4108
Patrick Fernandes De Faria <47286268+patrickf2a@users.noreply.github.com>
2024-01-27 02:22:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,4 +14,4 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/assignment-app/index.html"));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 10000);
+app.listen(process.env.PORT || 8010);
